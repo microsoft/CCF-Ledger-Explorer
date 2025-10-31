dffdf1d70a599889165294f14707e52b6d4cbb40
Ivar <iprudnikovas@microsoft.com>
2025-10-31 18:16:42 +0000

    add a test to check if scitt columns are rendered correctly

---
+++ e2e/table.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+import path from 'node:path';
+
+const testfilepath = import.meta.url.replace('file://', '');
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/files');
+  await page.getByRole('button', { name: 'Add Files' }).click();
+  await page.getByRole('button', { name: 'Select .committed Files' }).click();
+  await page.getByLabel('Upload CCF ledger files').setInputFiles([
+    path.join(testfilepath, '../test_files', 'ledger_1-14.committed'),
+    path.join(testfilepath, '../test_files', 'ledger_15-3926.committed'),
+  ]);
+  await expect(page.getByText('Total: 14 transactions')).toBeVisible();
+  // make sure file is fully processed
+  await expect(page.getByText('ledger_15-3926.committed')).toBeVisible();
+});
+
+test('shows scitt entry columns', async ({ page }) => {
+  await page.goto('/tables/public%3Ascitt.entry');
+  await expect(page.getByText('public:scitt.entry')).toBeVisible();
+
+  const table = page.getByRole('table').first();
+
+  await expect(table).toBeVisible();
+
+  const groups = table.getByRole('rowgroup');
+  const headerGroup = groups.first();
+  await expect(headerGroup.filter({ hasText: 'Sequence' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Transaction ID' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Key' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Value' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Issuer' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Subject' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Signed At' })).toBeVisible();
+  await expect(headerGroup.filter({ hasText: 'Actions' })).toBeVisible();
+
+  const bodyGroup = groups.nth(1);
+  const rowCount = await bodyGroup.getByRole('row').count();
+  expect(rowCount).toBeGreaterThan(10);
+});
